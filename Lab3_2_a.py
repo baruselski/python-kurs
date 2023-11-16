97bc7061d47d10b7799d008e99c592b0b523ef1b
bartek.zoladz <bartek.zoladz@gmail.com>
2023-11-16 13:01:58 +0100

    Finished 2a, starting 2b

---
+++ Lab3_2_a.py
@@ -0,0 +1,13 @@
+# Laboratorium 3 Zadanie 2 Podpunkt 'a'
+
+from datasets import load_dataset
+import pandas
+
+# Load dataset and convert to pandas dataframe
+dataset = load_dataset("imodels/credit-card")
+df = pandas.DataFrame(dataset['train'])
+
+# Remove duplicates based on all columns
+df_no_duplicates = df.drop_duplicates()
+
+#print(df_no_duplicates.to_string())
