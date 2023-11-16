c8844fc48c8fc8c34167ffff24c2b221bc8a977c
bartek.zoladz <bartek.zoladz@gmail.com>
2023-11-16 13:59:49 +0100

    solved3_2_e

--- Lab3_2_e.py
+++ Lab3_2_e.py
@@ -1,8 +1,33 @@
-# Laboratorium 3 Zadanie 2 Podpunkt 'c'
-
 from datasets import load_dataset
-import pandas
+import pandas as pd
+from tabulate import tabulate
 
-# Load dataset and convert to pandas dataframe
+# Load the dataset
 dataset = load_dataset("imodels/credit-card")
-df = pandas.DataFrame(dataset['train'])
+df = pd.DataFrame(dataset['train'])
+
+# Rename the education columns based on the provided key
+education_mapping = {
+    0: 'graduate school',
+    1: 'university',
+    2: 'high school',
+    3: 'others',
+    4: 'unknown',
+    5: 'unknown',
+    6: 'unknown'
+}
+
+df = df.rename(columns={f'education:{i}': f'Education:{education_mapping[i]}' for i in range(7) if f'education:{i}' in df.columns})
+
+# Find the 10 oldest clients
+oldest_clients = df.nlargest(10, 'age')
+
+# Sum all education
+oldest_clients['completed_education'] = oldest_clients.filter(like='Education:').sum(axis=1)
+
+# Select only the required columns
+selected_columns = ['limit_bal', 'age', 'Education:graduate school', 'Education:university', 'Education:high school', 'Education:others', 'Education:unknown', 'completed_education']
+oldest_clients = oldest_clients[selected_columns]
+
+# Print the resulting table
+print(tabulate(oldest_clients, headers = 'keys', tablefmt = 'psql'))
